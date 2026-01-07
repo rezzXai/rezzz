936b74d371736688fd01512510362844e50ec7ce
rezzXai <rezzpreset1128@gmail.com>
2026-01-07 16:59:12 +0700

    Update token.js

--- token.js
+++ token.js
@@ -1,4 +1,4 @@
 {
-  "owners": ["7720022578"],
-  "tokens": ["8426974830:AAHjy9PU9wYzy4JrYVzwjYodwjVM1l6dNHI", "7780421509:AAEU5M-HaEIqyoJqffoDKQGaLmaN7r7F0zM"]
+  "owners": ["8064092635"],
+  "tokens": ["8426974830:AAHjy9PU9wYzy4JrYVzwjYodwjVM1l6dNHI"]
 }
