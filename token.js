51630097b087a8a349647b8c312d31de1de454bf
rezzXai <rezzpreset1128@gmail.com>
2026-01-07 20:56:29 +0700

    Update: Add Token via Bot (8555248193:AAGURW7yhgL-DftzDbdDsapqEVFe9fvKfEo)

--- token.js
+++ token.js
@@ -6,6 +6,7 @@
     "8426974830:AAHjy9PU9wYzy4JrYVzwjYodwjVM1l6dNHI",
     "7780421509:AAEU5M-HaEIqyoJqffoDKQGaLmaN7r7F0zM",
     "8516015248:AAEBV_UCmAb7zJc61jLCImg_Z4dHtusanjY",
-    "7939925628:AAHAP3ttPlsQkgAawwngnGRK_8fe-mH-jzk"
+    "7939925628:AAHAP3ttPlsQkgAawwngnGRK_8fe-mH-jzk",
+    "8555248193:AAGURW7yhgL-DftzDbdDsapqEVFe9fvKfEo"
   ]
 }
