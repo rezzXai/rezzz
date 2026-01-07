b8d5269714fe670e2ccd2c6a160793a0d6c11bac
rezzXai <rezzpreset1128@gmail.com>
2026-01-07 17:09:11 +0700

    Update: Add Token via Bot (7780421509:AAEU5M-HaEIqyoJqffoDKQGaLmaN7r7F0zM)

--- token.js
+++ token.js
@@ -1,4 +1,9 @@
 {
-  "owners": ["8064092635"],
-  "tokens": ["8426974830:AAHjy9PU9wYzy4JrYVzwjYodwjVM1l6dNHI"]
+  "owners": [
+    "8064092635"
+  ],
+  "tokens": [
+    "8426974830:AAHjy9PU9wYzy4JrYVzwjYodwjVM1l6dNHI",
+    "7780421509:AAEU5M-HaEIqyoJqffoDKQGaLmaN7r7F0zM"
+  ]
 }
