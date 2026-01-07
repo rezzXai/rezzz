57a87eed24d6d930e7bb6f26ec75349124ac09dc
rezzXai <rezzpreset1128@gmail.com>
2026-01-07 16:23:01 +0700

    Refactor token.js to use object structure

--- token.js
+++ token.js
@@ -1,1 +1,4 @@
-["8426974830:AAHjy9PU9wYzy4JrYVzwjYodwjVM1l6dNHI","7780421509:AAEU5M-HaEIqyoJqffoDKQGaLmaN7r7F0zM"]
+{
+  "owners": ["7780421509:AAEU5M-HaEIqyoJqffoDKQGaLmaN7r7F0zM"],
+  "tokens": ["8426974830:AAHjy9PU9wYzy4JrYVzwjYodwjVM1l6dNHI"]
+}
