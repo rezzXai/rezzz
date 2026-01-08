a0ca6c6ff1bb81ea8a3b4c09106cc13f91c17224
rezzXai <rezzpreset1128@gmail.com>
2026-01-08 12:32:10 +0700

    Add Token: 8516015248:AAEBV_UCmAb7zJc61jLCImg_Z4dHtusanjY

--- token.js
+++ token.js
@@ -10,7 +10,8 @@
   "tokens": [
     "7780421509:AAEU5M-HaEIqyoJqffoDKQGaLmaN7r7F0zM",
     "8502995694:AAHocXIJc0wJKOo29h9bLFiHjIUVS06NhN0",
-    "8426974830:AAHjy9PU9wYzy4JrYVzwjYodwjVM1l6dNHI"
+    "8426974830:AAHjy9PU9wYzy4JrYVzwjYodwjVM1l6dNHI",
+    "8516015248:AAEBV_UCmAb7zJc61jLCImg_Z4dHtusanjY"
   ],
-  "sha": "a2ca7eea9f6f9ef41d80dd2b968f90774838d3c9"
+  "sha": "6467c939bb9a9e3e0c912dbae025c7dd61826086"
 }
