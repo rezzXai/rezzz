8d6e5eb28e70adaf1859a4fda5c4b030b85446ea
rezzXai <rezzpreset1128@gmail.com>
2026-01-08 11:13:07 +0700

    Update: Delete Token via Bot

--- token.js
+++ token.js
@@ -6,7 +6,6 @@
     "8426974830:AAHjy9PU9wYzy4JrYVzwjYodwjVM1l6dNHI",
     "7780421509:AAEU5M-HaEIqyoJqffoDKQGaLmaN7r7F0zM",
     "8516015248:AAEBV_UCmAb7zJc61jLCImg_Z4dHtusanjY",
-    "7939925628:AAHAP3ttPlsQkgAawwngnGRK_8fe-mH-jzk",
-    "8555248193:AAGURW7yhgL-DftzDbdDsapqEVFe9fvKfEo"
+    "7939925628:AAHAP3ttPlsQkgAawwngnGRK_8fe-mH-jzk"
   ]
 }
